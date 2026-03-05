698a0d67f2347254234ef3f501b94af43247133f
Androgorodok <androgorodok@gmail.com>
2026-03-05 11:40:41 +0300

    Исправлен тестовый файл

--- test.py
+++ test.py
@@ -8,18 +8,20 @@ if len(sys.argv) != 5:
 lib = ctypes.CDLL(sys.argv[1])
 key = int(sys.argv[2])
 
-lib.set_key.argtypes = [ctypes.c_int]
-lib.set_key(key)
+lib.cezare_key.argtypes = [ctypes.c_char]
+lib.cezare_key(chr(key).encode('latin1')[0])
 
-lib.transform.argtypes = [ctypes.c_void_p, ctypes.c_int]
+lib.cezare_enc.argtypes = [ctypes.c_void_p, ctypes.c_void_p, ctypes.c_int]
 
 with open(sys.argv[3], 'rb') as f:
     data = f.read()
 
-buf = ctypes.create_string_buffer(data, len(data))
-lib.transform(buf, len(data))
+in_buf = ctypes.create_string_buffer(data, len(data))
+out_buf = ctypes.create_string_buffer(len(data))
+
+lib.cezare_enc(in_buf, out_buf, len(data))
 
 with open(sys.argv[4], 'wb') as f:
-    f.write(buf.raw[:len(data)])
+    f.write(out_buf.raw[:len(data)])
 
 print(f"Done: {sys.argv[3]} -> {sys.argv[4]}")
